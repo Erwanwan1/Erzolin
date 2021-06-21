d5cf4692ab44a474af4817850861687feaaf8330
Erwanwan1 <erwanwan@outlook.fr>
2021-06-21 11:05:56 +0200

    Clean project

--- src/main/java/web/com/web/dao/UtilisateurDao.java
+++
@@ -1,9 +0,0 @@
-package web.com.web.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import web.com.web.model.Utilisateur;
-
-public interface UtilisateurDao extends JpaRepository<Utilisateur, Integer>{
-    
-}
